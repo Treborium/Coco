55290118e81e62fd9a6f9c36048474af8bbafa9e
Robert Fuchs <RobertFuchs97@gmail.com>
2019-04-28 15:25:41 +0200

    Define additional data files that need to be packaged to PyPi
    The mentioned files are:
    - database.json
    - coco.json

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="coco-cli",
-    version="2.0",
+    version="2.1",
     author="Robert Fuchs",
     author_email="RobertFuchs97@gmail.com",
     description="A CLI tool for managing your CLI tools",
@@ -20,6 +20,9 @@ setuptools.setup(
         'bullet',
         'click'
     ],
+    package_data={
+        'coco': ['database.json', 'coco.json']
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
