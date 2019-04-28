fd6f1a4ffd1d6b34d057a7889d278a5030ee3afb
Robert Fuchs <RobertFuchs97@gmail.com>
2019-04-28 11:36:17 +0200

    Create setup.py for shipping the package to PyPi

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="coco-cli",
+    version="2.0",
+    author="Robert Fuchs",
+    author_email="RobertFuchs97@gmail.com",
+    description="A CLI tool for managing your CLI tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Treborium/Coco",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': ['coco=coco.coco:cli'],
+    },
+    install_requires=[
+        'bullet',
+        'click'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
